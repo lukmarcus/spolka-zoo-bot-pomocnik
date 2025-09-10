917415d7940679e5cf1ceba1429b1985b3bff3ec
Marek Szumny <lukmarcus@gmail.com>
2025-09-10 23:26:07 +0200

    card name property removed

--- src/data/botCards.ts
+++ src/data/botCards.ts
@@ -7,7 +7,6 @@ import type { BotCard } from "../types";
 export const BOT_CARDS: BotCard[] = [
   {
     id: 1,
-    name: "", // Names removed in v0.3.4
     effects: [
       "Kup najtańszy <span style='color: #FFFFFF; font-weight: bold;'>OWOC</span> z <span style='color: #FFFFFF; font-weight: bold;'>MAGAZYNU</span>. Jeśli danego <span style='color: #FFFFFF; font-weight: bold;'>OWOCU</span> nie ma w <span style='color: #FFFFFF; font-weight: bold;'>MAGAZYNIE</span>, odkup go od innego gracza (pierwszego z lewej, który ma ten <span style='color: #FFFFFF; font-weight: bold;'>OWOC</span> w swojej puli).",
       "Kup najtańszy <span style='color: #FFFFFF; font-weight: bold;'>OWOC</span> z <span style='color: #FFFFFF; font-weight: bold;'>MAGAZYNU</span>. Jeśli danego <span style='color: #FFFFFF; font-weight: bold;'>OWOCU</span> nie ma w <span style='color: #FFFFFF; font-weight: bold;'>MAGAZYNIE</span>, odkup go od innego gracza (pierwszego z lewej, który ma ten <span style='color: #FFFFFF; font-weight: bold;'>OWOC</span> w swojej puli).",
@@ -17,7 +16,6 @@ export const BOT_CARDS: BotCard[] = [
   },
   {
     id: 2,
-    name: "",
     effects: [
       "Zatrudnij <span style='color: #D14019; font-weight: bold;'>POMOCNIKA</span>.",
       "Przełóż jawnie wierzchnią <img src='/src/assets/images/interface/card.png' alt='karta' class='card-icon' /> z <span style='color: #77812B; font-weight: bold;'>TALII SPÓŁKI</span> <img src='/src/assets/images/interface/lemon.png' alt='cytryna' class='card-icon' /> na wierzch <span style='color: #77812B; font-weight: bold;'>TALII SPÓŁKI</span> <img src='/src/assets/images/interface/pineapple.png' alt='ananas' class='card-icon' />.",
@@ -27,7 +25,6 @@ export const BOT_CARDS: BotCard[] = [
   },
   {
     id: 3,
-    name: "",
     effects: [
       "Zatrudnij <span style='color: #D14019; font-weight: bold;'>POMOCNIKA</span>.",
       "Przełóż jawnie wierzchnią <img src='/src/assets/images/interface/card.png' alt='karta' class='card-icon' /> z <span style='color: #77812B; font-weight: bold;'>TALII SPÓŁKI</span> <img src='/src/assets/images/interface/apple.png' alt='jabłko' class='card-icon' /> na wierzch <span style='color: #77812B; font-weight: bold;'>TALII SPÓŁKI</span> <img src='/src/assets/images/interface/lemon.png' alt='cytryna' class='card-icon' />.",
@@ -37,7 +34,6 @@ export const BOT_CARDS: BotCard[] = [
   },
   {
     id: 4,
-    name: "",
     effects: [
       "Zatrudnij <span style='color: #D14019; font-weight: bold;'>POMOCNIKA</span>.",
       "Przełóż jawnie wierzchnią <img src='/src/assets/images/interface/card.png' alt='karta' class='card-icon' /> z <span style='color: #77812B; font-weight: bold;'>TALII SPÓŁKI</span> <img src='/src/assets/images/interface/pineapple.png' alt='ananas' class='card-icon' /> na wierzch <span style='color: #77812B; font-weight: bold;'>TALII SPÓŁKI</span> <img src='/src/assets/images/interface/apple.png' alt='jabłko' class='card-icon' />.",
@@ -47,7 +43,6 @@ export const BOT_CARDS: BotCard[] = [
   },
   {
     id: 5,
-    name: "",
     effects: [
       "Sprzedaj ze swojej puli <span style='color: #FFFFFF; font-weight: bold;'>OWOC</span> kosztujący 3<img src='/src/assets/images/interface/money.png' alt='moneta' class='card-icon' /> lub więcej.",
       "Usuń wszystkie odkryte karty <span style='color: #77812B; font-weight: bold;'>TALII WPŁYWU</span>. Następnie uzupełnij <span style='color: #77812B; font-weight: bold;'>TALIĘ WPŁYWU</span>.",
@@ -57,7 +52,6 @@ export const BOT_CARDS: BotCard[] = [
   },
   {
     id: 6,
-    name: "",
     effects: [
       "Kup najtańszy <span style='color: #E76C33; font-weight: bold;'>UDZIAŁ</span>.",
       "Kup najtańszy <span style='color: #E76C33; font-weight: bold;'>UDZIAŁ</span>.",
@@ -67,7 +61,6 @@ export const BOT_CARDS: BotCard[] = [
   },
   {
     id: 7,
-    name: "",
     effects: [
       "Kup <span style='color: #E76C33; font-weight: bold;'>UDZIAŁ</span> w <span style='color: #E76C33; font-weight: bold;'>SPÓŁCE</span> <img src='/src/assets/images/interface/pineapple.png' alt='ananas' class='card-icon' />, jeśli jego cena wynosi 8<img src='/src/assets/images/interface/money.png' alt='moneta' class='card-icon' /> lub mniej.",
       "Kup <span style='color: #E76C33; font-weight: bold;'>UDZIAŁ</span> w <span style='color: #E76C33; font-weight: bold;'>SPÓŁCE</span> <img src='/src/assets/images/interface/pineapple.png' alt='ananas' class='card-icon' />, jeśli jego cena wynosi 8<img src='/src/assets/images/interface/money.png' alt='moneta' class='card-icon' /> lub mniej.",
@@ -77,7 +70,6 @@ export const BOT_CARDS: BotCard[] = [
   },
   {
     id: 8,
-    name: "",
     effects: [
       "Kup <span style='color: #E76C33; font-weight: bold;'>UDZIAŁ</span> w <span style='color: #E76C33; font-weight: bold;'>SPÓŁCE</span> <img src='/src/assets/images/interface/lemon.png' alt='cytryna' class='card-icon' />, jeśli jego cena wynosi 8<img src='/src/assets/images/interface/money.png' alt='moneta' class='card-icon' /> lub mniej.",
       "Kup <span style='color: #E76C33; font-weight: bold;'>UDZIAŁ</span> w <span style='color: #E76C33; font-weight: bold;'>SPÓŁCE</span> <img src='/src/assets/images/interface/lemon.png' alt='cytryna' class='card-icon' />, jeśli jego cena wynosi 8<img src='/src/assets/images/interface/money.png' alt='moneta' class='card-icon' /> lub mniej.",
@@ -87,7 +79,6 @@ export const BOT_CARDS: BotCard[] = [
   },
   {
     id: 9,
-    name: "",
     effects: [
       "Kup <span style='color: #E76C33; font-weight: bold;'>UDZIAŁ</span> w <span style='color: #E76C33; font-weight: bold;'>SPÓŁCE</span> <img src='/src/assets/images/interface/apple.png' alt='jabłko' class='card-icon' />, jeśli jego cena wynosi 8<img src='/src/assets/images/interface/money.png' alt='moneta' class='card-icon' /> lub mniej.",
       "Kup <span style='color: #E76C33; font-weight: bold;'>UDZIAŁ</span> w <span style='color: #E76C33; font-weight: bold;'>SPÓŁCE</span> <img src='/src/assets/images/interface/apple.png' alt='jabłko' class='card-icon' />, jeśli jego cena wynosi 8<img src='/src/assets/images/interface/money.png' alt='moneta' class='card-icon' /> lub mniej.",
@@ -97,7 +88,6 @@ export const BOT_CARDS: BotCard[] = [
   },
   {
     id: 10,
-    name: "",
     effects: [
       "Sprzedaj ze swojej puli <span style='color: #FFFFFF; font-weight: bold;'>OWOC</span> kosztujący 2<img src='/src/assets/images/interface/money.png' alt='moneta' class='card-icon' /> lub mniej.",
       "Usuń wszystkie odkryte karty <span style='color: #77812B; font-weight: bold;'>TALII WPŁYWU</span>. Następnie uzupełnij <span style='color: #77812B; font-weight: bold;'>TALIĘ WPŁYWU</span>.",
@@ -107,7 +97,6 @@ export const BOT_CARDS: BotCard[] = [
   },
   {
     id: 11,
-    name: "",
     effects: [
       "Sprzedaj ze swojej puli <span style='color: #FFFFFF; font-weight: bold;'>OWOC</span> kosztujący 4<img src='/src/assets/images/interface/money.png' alt='moneta' class='card-icon' /> lub więcej.",
       "Usuń wszystkie odkryte karty <span style='color: #77812B; font-weight: bold;'>TALII WPŁYWU</span>. Następnie uzupełnij <span style='color: #77812B; font-weight: bold;'>TALIĘ WPŁYWU</span>.",
@@ -117,7 +106,6 @@ export const BOT_CARDS: BotCard[] = [
   },
   {
     id: 12,
-    name: "",
     effects: [
       "Jeśli nie jesteś pierwszym graczem, dobierz wierzchnią kartę z <span style='color: #77812B; font-weight: bold;'>TALII WPŁYWU</span>. Pokaż ją wszystkim graczom, a następnie wtasuj do <span style='color: #77812B; font-weight: bold;'>TALII Spółki</span> <img src='/src/assets/images/interface/lemon.png' alt='cytryna' class='card-icon' />.",
       "Jeśli nie jesteś pierwszym graczem, dobierz wierzchnią kartę z <span style='color: #77812B; font-weight: bold;'>TALII WPŁYWU</span>. Pokaż ją wszystkim graczom, a następnie wtasuj do <span style='color: #77812B; font-weight: bold;'>TALII Spółki</span> <img src='/src/assets/images/interface/pineapple.png' alt='ananas' class='card-icon' />.",
@@ -127,7 +115,6 @@ export const BOT_CARDS: BotCard[] = [
   },
   {
     id: 13,
-    name: "",
     effects: [
       "Sprzedaj swój najdroższy <span style='color: #E76C33; font-weight: bold;'>UDZIAŁ</span>.",
       "Sprzedaj swój najdroższy <span style='color: #E76C33; font-weight: bold;'>UDZIAŁ</span>.",
